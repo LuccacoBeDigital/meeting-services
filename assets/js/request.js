2b05e8faa16ce25aaaa76a1d483aa37b4eb181dd
Carlos Gustavo <froskie@gmail.com>
2017-10-24 07:36:27 +0100

    refactor of js and css

---
+++ assets/js/request.js
@@ -0,0 +1,67 @@
+/*eslint-env jquery*/
+$(function() {
+
+  var $headerDropdown = $('.headerDropdown');
+  var $headerDropdownToggle = $('.headerDropdownToggle');
+
+  function hasClickedOutside() {
+    $headerDropdown.addClass('hidden');
+    $(window).off('click', hasClickedOutside);
+  }
+  
+  $headerDropdownToggle.on('click', function(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    if ($headerDropdown.hasClass('hidden')) {
+      $(window).on('click', hasClickedOutside);
+      $headerDropdown.removeClass('hidden');
+    } else {
+      hasClickedOutside();
+    }
+  });
+
+  // enable tooltips
+  $('[data-toggle="tooltip"]').tooltip();
+
+  // date pickers
+  $('.datepicker').datetimepicker({
+    format: 'mm/dd/yy',
+    minView: 2,
+    autoclose: true
+  });
+  $('.timepicker').datetimepicker({
+    format: 'hh:ii',
+    startView: 1,
+    maxView: 1,
+    autoclose: true
+  });
+
+  function setupToggleArea($area) {
+    var $header = $area.find('> header');
+    var $section = $area.find('> section');
+
+    var $toggleButton = $header.find('> button');
+    var $toggleArrow = $header.find('> a');
+    var $toggleSwitch = $header.find('[data-elm]');
+
+    $toggleSwitch.on('click', function(e) {
+      e.preventDefault();
+      var isClosed = $section.hasClass('hidden');
+      if (isClosed) {
+        $toggleArrow.find('i')
+                    .removeClass('fa-chevron-down')
+                    .addClass('fa-chevron-up');
+      } else {
+        $toggleArrow.find('i')
+                    .removeClass('fa-chevron-up')
+                    .addClass('fa-chevron-down');
+      }
+      $section.toggleClass('hidden');
+      $toggleButton.toggleClass('active');
+    });
+  }
+
+  $(".toggle-area").each(function(index, elm){
+    setupToggleArea($(elm));
+  });
+});
